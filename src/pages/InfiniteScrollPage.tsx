c078fbd4d7ec3babefbd13467d041e146006933c
YongHoon <akatcn@gmail.com>
2023-03-11 20:16:26 +0900

    무한 스크롤 로딍중에도 여러번 요청되는 현상 useRef로 해결

--- src/pages/InfiniteScrollPage.tsx
+++ src/pages/InfiniteScrollPage.tsx
@@ -1,38 +1,42 @@
-// callback 함수 내에서 isLoading의 변화를 전혀 감지해내지 못하고 있다. 아마 콜백함수의 특성상 값을 capturing하는 동작때문에 그런 것 같다. 이를 해결할 수 있어야 한다
-
 import React, { useEffect, useRef, useState } from 'react'
 
 function InfiniteScrollPage() {
   const [imageURLs, setImageURLs] = useState<string[]>([""]);  // 이미지의 URL을 담을 state 변수
   const [isLoading, setIsLoading] = useState(false);  // 현재 로딩중이면 true, 그렇지 않다면 false
-  const [num, setNum] = useState(0);
+  const isLoadingRef = useRef(isLoading);
   const lastDivRef = useRef(null);  // 마지막 div 태그를 참조할 ref 객체
 
   const addImageURL = async () => {
-    // setNum은 정상 동작하여 num++을 하게된다
-    setNum((oldState) => {
-      return oldState + 1;
+    // 로딩중 상태로 변경
+    setIsLoading(() => {
+      console.log("로딩 시작...");
+      isLoadingRef.current = true;
+      return true;
     });
-    console.log("요청함...", num);  // 여기도 항상 0으로 찍힌다
-    
-    setIsLoading(true);  // 로딩중 상태로 변경
+
+    // 이미지 요청
     const response = await fetch('https://dog.ceo/api/breeds/image/random');
     const data = await response.json();
     const imageURL = data.message;
-    // console.log("로드할 이미지의 URL:", imageURL);
-    setIsLoading(false);  // 로딩 완료되었으므로 false로 세팅
+
+    // 요청 완료되었으므로 false로 세팅
+    setIsLoading(() => {
+      console.log("로딩 끝!", imageURL);
+      isLoadingRef.current = false;
+      return false;
+    });
     
+    // 이미지 URL 추가하기
     setImageURLs((oldState) => {
       return [...oldState, imageURL]
     })
   }
 
   useEffect(() => {
-    // 트리거 될 때의 콜백 함수
+    // 트리거 될 때의 콜백 함수 정의
     const callback = (entries: IntersectionObserverEntry[]) => {
-      console.log("callback's number:", num);  // 왜 여기가 항상 0으로 찍힐까?? 아마 콜백 호출을 함에 따라 값이 capturing 되는 것 같다
       entries.forEach(entry => {
-        if (entry.isIntersecting && isLoading === false) {  // 현재 로딩중인 상태에서도 마지막에 도달할 수 있으므로 로딩중이지 않을 경우(=isLoading이 false)인 경우에만 요청을 보내야 한다
+        if (entry.isIntersecting && isLoadingRef.current === false) {  // 현재 로딩중인 상태에서도 마지막에 도달할 수 있으므로 로딩중이지 않을 경우(=isLoading이 false)인 경우에만 요청을 보내야 한다
           addImageURL();
         }
       })
@@ -45,7 +49,6 @@ function InfiniteScrollPage() {
 
   return (
     <div>
-      <button className="bg-red-300" onClick={() => alert(num)}>클릭</button> {/* num을 확인하기 위한 더미 버튼 */}
       {imageURLs.map((imageURL: string, index) => {
         if (index === imageURLs.length-1) {
           return <img key={index} className="border-4 border-red-500" src={imageURL} />  // 마지막 이미지에는 빨간 테두리를 붙여준다
